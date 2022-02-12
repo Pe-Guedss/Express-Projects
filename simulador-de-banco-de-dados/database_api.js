ea2272bb61e5db651467242087537d065a310fdd
Pe-Guedss <pedro.og2002@gmail.com>
2022-02-12 01:25:29 -0300

    Removendo as exportações do objeto dataBase.

--- simulador-de-banco-de-dados/database_api.js
+++ simulador-de-banco-de-dados/database_api.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express ();
-var dataBase = require("./data/dataBase");
 
 app.use (express.json());
 
@@ -13,8 +12,6 @@ app.use("/create", createRouter);
 app.use("/delete", deleteRouter);
 app.use("/read", readRouter);
 app.use("/update", updateRouter);
-
-console.log();
 
 app.listen (3000, () => {
     console.log("Server started on port 3000.");
