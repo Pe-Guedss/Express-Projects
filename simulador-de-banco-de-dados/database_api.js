aa85acefe5d54eb15004a8f80e34b62613807868
Pe-Guedss <pedro.og2002@gmail.com>
2022-02-12 00:30:17 -0300

    Criando rotas para a API e as incluindo no arquivo principal.

--- simulador-de-banco-de-dados/database_api.js
+++ simulador-de-banco-de-dados/database_api.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express ();
 var dataBase = require("./data/dataBase");
 
 app.use (express.json());
 
-console.log(dataBase);
+const createRouter = require("./routes/create");
+const deleteRouter = require("./routes/delete");
+const readRouter = require("./routes/read");
+const updateRouter = require("./routes/update");
 
-// app.get("/select")
+app.use("/create", createRouter);
+app.use("/delete", deleteRouter);
+app.use("/read", readRouter);
+app.use("/update", updateRouter);
+
+console.log();
 
 app.listen (3000, () => {
     console.log("Server started on port 3000.");
---
+++ simulador-de-banco-de-dados/routes/delete.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/', (req, res)=> {
+    res.send("Implementar rota Delete!")
+});
+
+module.exports = router;
