c85db9583e0ff8ba22ffb6f3d7ed3f0c99091d53
Pe-Guedss <pedro.og2002@gmail.com>
2022-02-19 21:35:09 -0300

    Implementando requests "new_table" e "insert".

--- simulador-de-banco-de-dados/routes/create/create.js
+++ simulador-de-banco-de-dados/routes/create/create.js
@@ -7,4 +7,28 @@ router.get("/", (req, res)=> {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+router.post("/new_table/:tableName", (req, res) => {
+    const { tableName } = req.params;
+
+    dataBase[tableName] = {};
+
+    res.status(200).send(dataBase);
+});
+
+router.post("/insert/:tableName", (req, res) => {
+    const { tableName } = req.params;
+    const { elementId, properties } = req.body;
+
+    if (!elementId || !properties) {
+        res.status(500).send("Bad Request! Either 'elementID' or 'properties' are missing of misspelled.")
+    }
+    else if (!dataBase[tableName]) {
+        res.status(404).send("Table not found!")
+    }
+    else {
+        dataBase[tableName][elementId] = properties;
+        res.status(200).send(dataBase[tableName])
+    }
+});
+
 module.exports = router;
