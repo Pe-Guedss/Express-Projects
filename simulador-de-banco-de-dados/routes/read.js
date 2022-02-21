2b33c6f70e320c49c833d9f485c204ed1668de27
Matheus Henrique dos Santos Lima <1995.matheus.lima@gmail.com>
2022-02-20 22:17:01 -0300

    Finalizado o desenvolvimento de requisições Get do READ

--- simulador-de-banco-de-dados/routes/read.js
+++ simulador-de-banco-de-dados/routes/read.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 
+const res = require('express/lib/response');
 var dataBase = require("../data/dataBase");
 
 router.get('/', (req, res)=> {
-    res.send("Implementar rota READ!")
+    res.send(dataBase.lojaDeJogos)
 });
+
+router.get('/:jogo', (req, res) =>{
+    const Jogo = req.params.jogo;
+    res.send(dataBase.lojaDeJogos[Jogo]);
+})
+
+router.get('/:jogo/:propriedade' , (req, res) => {
+    const {jogo, propriedade} = req.params;
+    res.send(dataBase.lojaDeJogos[jogo][propriedade]);
+})
 
 module.exports = router;
