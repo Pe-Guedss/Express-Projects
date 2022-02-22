f59e1cc1f75977c2a9683fc85565b07144abcf70
daniel-andradec <danielandradecarmo@gmail.com>
2022-02-22 20:18:06 -0300

    Rota de edição

--- simulador-de-banco-de-dados/routes/update.js
+++ simulador-de-banco-de-dados/routes/update.js
@@ -6,4 +6,21 @@ router.get('/', (req, res)=> {
     res.send("Implementar rota UPDATE!")
 });
 
+router.put('/:tableName/:id', (req, res)=> {
+    const {tableName, id} = req.params; 
+    const jogo = dataBase[tableName][id];
+    const body = req.body;
+    if(jogo){
+        dataBase[tableName][id].nome = body.nome||jogo.nome;
+        dataBase[tableName][id].ano = body.ano||jogo.ano;
+        dataBase[tableName][id].preco = body.preco||jogo.preco;
+        dataBase[tableName][id].genero = body.genero||jogo.genero;
+    
+        res.json(dataBase[tableName]);
+    }else{
+        res.status(404).send("Game not found!");
+    }
+    
+});
+
 module.exports = router;
