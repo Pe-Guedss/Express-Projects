22c92687c0af4a17fceb1ebf6f742a26562fff9f
Breno Henrique <breno.henriquefacul@gmail.com>
2022-02-22 21:21:09 -0300

    Rota Delete

--- simulador-de-banco-de-dados/routes/delete.js
+++ simulador-de-banco-de-dados/routes/delete.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 
+const { lojaDeJogos } = require('../data/dataBase');
 var dataBase = require("../data/dataBase");
 
-router.get('/', (req, res)=> {
+router.delete("/delete_all", (req, res)=> {
+    
+    for(var item in dataBase.lojaDeJogos){
+         dataBase.lojaDeJogos = {};
+    }
+    res.send("Implementar rota DELETE!")
+});
+
+router.delete("/:tableName", (req, res)=> {
+    
+    const { tableName } = req.params;
+    delete dataBase.lojaDeJogos[tableName];
+   
     res.send("Implementar rota DELETE!")
 });
 
