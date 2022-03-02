e05d3af88cb338ca22003665efa6ae3c066237e0
Breno Henrique <breno.henriquefacul@gmail.com>
2022-03-02 15:07:36 -0300

    rota delete

--- simulador-de-banco-de-dados/routes/delete.js
+++ simulador-de-banco-de-dados/routes/delete.js
@@ -1,22 +1,45 @@
 const router = require('express').Router();
 
-const { lojaDeJogos } = require('../data/dataBase');
 var dataBase = require("../data/dataBase");
 
-router.delete("/delete_all", (req, res)=> {
-    
-    for(var item in dataBase.lojaDeJogos){
-         dataBase.lojaDeJogos = {};
-    }
-    res.send("Implementar rota DELETE!")
+router.delete("/", (req, res)=> {
+   for(let table in dataBase) {
+       delete dataBase[table];
+   }
+    res.status(200).send('All in dataBase Was Delete');
 });
 
 router.delete("/:tableName", (req, res)=> {
-    
-    const { tableName } = req.params;
-    delete dataBase.lojaDeJogos[tableName];
-   
-    res.send("Implementar rota DELETE!")
+    const tableName = req.params.tableName;
+    if(!dataBase[tableName]){
+        res.status(404).send(`${tableName} Not Found`);
+    }
+    else{
+        delete dataBase[tableName];
+        res.status(200).send(`${tableName} was delete successfuly`)
+    }
+});
+
+router.delete("/:tableName/:id", (req, res)=> {
+    const {tableName, id} = req.params;
+    if(!dataBase[tableName][id]){
+        res.status(404).send(` ${id} Not Found in ${tableName}`);
+    }
+    else{
+        delete dataBase[tableName][id];
+        res.status(200).send(`${tableName}/${id}  was delete successfully`)
+    }
+});
+
+router.delete("/:tableName/:id/:property", (req, res)=> {
+    const {tableName, id,property} = req.params;
+    if(!dataBase[tableName][id][property]){
+        res.status(404).send(`${id} was no property called "${property}"`);
+    }
+    else{
+        delete dataBase[tableName][id][property];
+        res.status(200).send(`${tableName}/${id}/${property}  was delete successfully`)
+    }
 });
 
 module.exports = router;
