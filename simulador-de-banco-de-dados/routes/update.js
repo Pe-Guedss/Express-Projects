33ac95ae87689e449c31448e655aa49755d36a4a
daniel-andradec <danielandradecarmo@gmail.com>
2022-03-04 17:47:16 -0300

    Rota update

--- simulador-de-banco-de-dados/routes/update.js
+++ simulador-de-banco-de-dados/routes/update.js
@@ -2,25 +2,76 @@ const router = require('express').Router();
 
 var dataBase = require("../data/dataBase");
 
-router.get('/', (req, res)=> {
-    res.send("Implementar rota UPDATE!")
+
+// para varias propriedades de varios ids que estão em uma loja especifica
+router.put('/:tableName', (req, res)=> {
+    //enviar um json 
+    const tableName = req.params.tableName; 
+    const body = req.body;
+    console.log(body);
+    if(!dataBase[tableName]){
+        res.status(404).send(`${tableName} not found!`);
+    }
+    else{
+        for( const id in body){
+            if(!dataBase[tableName][id]){
+                res.status(404).send(`${id} not found!`);
+            }
+            else{
+                for(let property in body[id]){
+                    if(!dataBase[tableName][id][property]){
+                        delete body[id][property];
+                    }
+                }
+            }
+        }
+        for( let id in body){
+            console.log(id)
+
+            for(let property in body[id]){
+                dataBase[tableName][id][property] = body[id][property];
+                res.status(200).send(`everthing was updated sucessfuly`)
+                
+            }
+        }
+    }
 });
 
+// Para varias propriedades de um id dentro de uma loja especifica
 router.put('/:tableName/:id', (req, res)=> {
     const {tableName, id} = req.params; 
-    const jogo = dataBase[tableName][id];
     const body = req.body;
-    if(jogo){
-        dataBase[tableName][id].nome = body.nome||jogo.nome;
-        dataBase[tableName][id].ano = body.ano||jogo.ano;
-        dataBase[tableName][id].preco = body.preco||jogo.preco;
-        dataBase[tableName][id].genero = body.genero||jogo.genero;
-    
-        res.json(dataBase[tableName]);
-    }else{
-        res.status(404).send("Game not found!");
+    if(!dataBase[tableName][id]){
+        res.status(404).send(`${id} not found!`);
+    }
+    else{
+        for(let property in body){
+            if(!dataBase[tableName][id][property]){
+                res.status(404).send(`property called ${property} not found`);
+            }
+        }
+        for(let property in body){
+           dataBase[tableName][id][property] = body[property]; 
+        }
+        res.status(200).send(`${id}  was  updated successfully`)
     }
     
+});
+
+// Para uma propriedade, de um id que esta dentro de uma loja especifica
+router.put('/:tableName/:id/:property', (req, res) => {
+    const {tableName, id, property} = req.params;
+    const body = req.body;
+    if(!dataBase[tableName][id][property]){
+        res.status(404).send(`Property "${property}"  not found`);
+    }
+    else if(!dataBase[tableName][id][Object.keys(body)]){
+        res.status(404).send(`Property "${Object.keys(body)}" in body not found in ${id}`);
+    }
+    else{
+        dataBase[tableName][id][property] = body[property] || dataBase[tableName][id][property];
+        res.status(200).send(`property "${property}" was  updated successfully `)
+    }
 });
 
 module.exports = router;
