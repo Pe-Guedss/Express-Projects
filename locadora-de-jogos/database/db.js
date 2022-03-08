b2cec4a382983d5b642838f7159014b9fb3a8480
Pe-Guedss <pedro.og2002@gmail.com>
2022-03-07 21:21:51 -0300

    Consertando chamada assíncrona de autenticação do sequelize.

--- locadora-de-jogos/database/db.js
+++ locadora-de-jogos/database/db.js
@@ -2,15 +2,15 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize({ 
     dialect: 'sqlite',
     storage: './database/database.sqlite'
-})
+});
 
-(async () => {
+async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully!');
     } catch (error) {
         console.error('Unable to connect to the database due to: \n', error);
     }
-})();
+}
 
 module.exports = sequelize;
