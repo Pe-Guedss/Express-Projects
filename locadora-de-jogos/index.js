ead72345dae9d610003ed4024625f57599d79782
Pe-Guedss <pedro.og2002@gmail.com>
2022-03-07 21:59:25 -0300

    Trocando a porta de serviço para 3000.

--- locadora-de-jogos/index.js
+++ locadora-de-jogos/index.js
@@ -6,14 +6,13 @@ app.use (express.urlencoded({
     extended: false,
 }));
 
-const userRouter = require("./routes/userRoutes");
-const gameRouter = require("./routes/gameRoutes");
+const userRouter = require('./routes/userRoutes');
+const gameRouter = require('./routes/gameRoutes');
 
-app.use("/users", userRouter);
-app.use("/games", gameRouter);
+app.use('/users', userRouter);
+app.use('/games', gameRouter);
 
 
-app.listen (3001, () => {
-    console.log("Server started on port 3001.");
+app.listen (3000, () => {
+    console.log('Server started on port 3000.');
 });
-
