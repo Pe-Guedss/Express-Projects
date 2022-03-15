5ad5b9d53c2fed4fd9b22140597d215f895ecbc4
daniel-andradec <danielandradecarmo@gmail.com>
2022-03-15 18:09:46 -0300

    Middleware de validação do usuário
    Nessa versão implementamos um middleware de validação do usuário no index.js, antes da entrada para as rotas.
    
    - Se o request não contiver credenciais, é criado um novo usuário, desde que o email não exista previamente na base.
    
    - Se for feito um request do tipo PUT, só será permitida a alteração de dados de um usuário caso seja ele mesmo tentando mudar seu próprio registro.

--- locadora-de-jogos-criptografada/index.js
+++ locadora-de-jogos-criptografada/index.js
@@ -1,10 +1,48 @@
 const express = require('express');
 const app = express ();
+const bcrypt = require('bcrypt');
 
 app.use (express.json());
 app.use (express.urlencoded({
     extended: false,
 }));
+
+const Usuarios = require('./models/Usuario'); 
+
+app.use(async (req, res, next) => {
+
+    try {
+        const { creds } = req.body;
+        if(!creds){
+            next()
+            return;
+        }
+        const { email, senha } = creds;
+
+        const users = await Usuarios.findAll({where: {
+            email: email
+        }});
+
+        if (users.length === 0) {
+            res.status(404).send("User not found.");
+            return;
+        }
+        const dataBaseSenha = users[0].dataValues.senha;
+        const autenticacao = await bcrypt.compare(senha, dataBaseSenha);
+        if(autenticacao){
+            next()
+            return;
+        }
+        else{
+            res.status(400).send("Wrong password!");
+            return;
+        }
+    } 
+    catch (err) {
+        next(err);
+        return;
+    }
+});
 
 const userRouter = require('./routes/userRoutes');
 const gameRouter = require('./routes/gameRoutes');
